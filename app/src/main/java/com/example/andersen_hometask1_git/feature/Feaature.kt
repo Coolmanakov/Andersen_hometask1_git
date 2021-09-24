457e423123766e28a54add6a4286474c080a114f
Coolmanakov <kulmanakovdenis@yandex.ru>
2021-09-24 12:53:36 +0300

    Commit on branch feature

---
+++ app/src/main/java/com/example/andersen_hometask1_git/feature/Feaature.kt
@@ -0,0 +1,5 @@
+package com.example.andersen_hometask1_git.feature
+
+data class Feature(
+    val id : Number?
+)
---
+++ app/src/main/java/com/example/andersen_hometask1_git/feature/FeatureClass.kt
@@ -0,0 +1,4 @@
+package com.example.andersen_hometask1_git.feature
+
+class FeatureClass {
+}
